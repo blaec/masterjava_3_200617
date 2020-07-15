810473a097567d29e996876a979a0b56bfc3cd8e
blaec <odeskonst@yandex.ru>
2020-07-15 22:46:06 +0300

    fix wsdl

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class MailServicePublisher {
 
     public static void main(String[] args) {
+//        Endpoint.publish("http://localhost:8080/mail/mailService", new MailServiceImpl());
         DBITestProvider.initDBI();
 
         Endpoint endpoint = Endpoint.create(new MailServiceImpl());
