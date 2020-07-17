e028c81fbcc9eadae64d8bb527734b38fe98888d
blaec <odeskonst@yandex.ru>
2020-07-17 23:01:26 +0300

    send email via webapp ui

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -4,13 +4,22 @@ import com.google.common.collect.ImmutableMap;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -23,5 +32,23 @@ public class UsersServlet extends HttpServlet {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String subject = req.getParameter("subject");
+        String body = req.getParameter("body");
+        List<String> toList = Arrays.asList(req.getParameterValues("toList"));
+        Set<Addressee> addresseeList = userDao.getWithLimit(20).stream()
+                .filter(u -> toList.contains(u.getId().toString()))
+                .map(u -> new Addressee(u.getEmail(), u.getFullName()))
+                .collect(Collectors.toSet());
+
+        Service service = Service.create(
+                new URL("http://localhost:8080/mail/mailService?wsdl"),
+                new QName("http://mail.javaops.ru/", "MailServiceImplService"));
+
+        MailService mailService = service.getPort(MailService.class);
+        mailService.sendToGroup(addresseeList, null, subject, body);
     }
 }
